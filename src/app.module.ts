c3cb0768323c7f1fb83cf8b4be921477da73935a
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-03 16:23:02 +0300

    refactor : app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,27 +16,33 @@ import { InstructorEntity } from './instructor/entities/instructor.entity';
 import { SummertrainingEntity } from './summertraining/entities/summertraining.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [AuthModule, UserModule, CourseModule, FaqsModule, InstructorModule, ReviewModule, SummertrainingModule,
     ConfigModule.forRoot({
     isGlobal: true,
   }),
-  TypeOrmModule.forRoot({
-    type: 'mongodb',
-    url: 'mongodb+srv://mahmoudammar560:ammar@2024@cluster0.6niiy.mongodb.net/plan-b?retryWrites=true&w=majority',
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    synchronize: true, // Make sure you use this only in development
-    entities: [
-      UserEntity,
-      CourseEntity,
-      FaqEntity,
-      ReviewEntity,
-      InstructorEntity,
-      SummertrainingEntity,
-    ],
+  // TypeOrmModule.forRoot({
+  //   type: 'mongodb',
+  //   url: 'mongodb+srv://mahmoudammar560:ammar@2024@cluster0.6niiy.mongodb.net/plan-b?retryWrites=true&w=majority',
+  //   useUnifiedTopology: true,
+  //   useNewUrlParser: true,
+  //   synchronize: true, // Make sure you use this only in development
+  //   entities: [
+  //     UserEntity,
+  //     CourseEntity,
+  //     FaqEntity,
+  //     ReviewEntity,
+  //     InstructorEntity,
+  //     SummertrainingEntity,
+  //   ],
+  // }),
+  MongooseModule.forRoot('mongodb+srv://mahmoudammar560:ammar@2024@cluster0.6niiy.mongodb.net/plan-b?retryWrites=true&w=majority', {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   }),
+  
 ],
   controllers: [AppController],
   providers: [AppService],
