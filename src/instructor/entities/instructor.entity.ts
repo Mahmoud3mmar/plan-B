08480cebcb2456af6fd1fb07e5335b6cc83768f0
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-03 17:51:25 +0300

    refactor : instructor entity

--- src/instructor/entities/instructor.entity.ts
+++ src/instructor/entities/instructor.entity.ts
@@ -1,1 +1,22 @@
-export class InstructorEntity {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class Instructor extends Document {
+  @Prop({ required: true, length: 255 })
+  name: string;
+
+  @Prop({ required: true, type: String })
+  bio: string;
+
+  @Prop({ type: [String], default: [] })
+  socialMediaUrls: string[];
+
+  @Prop({ type: Number, default: 0 })
+  numberOfCourses: number;
+
+//   @Prop({ type: [{ type: Types.ObjectId, ref: Course.name }], default: [] })
+//   courses: Types.ObjectId[]; // Array of references to Course documents
+}
+
+export const InstructorSchema = SchemaFactory.createForClass(Instructor);
