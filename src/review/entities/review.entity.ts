e4e79987ee46a8fe89214d085fc2c8ded1593fd0
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-03 17:51:36 +0300

    refactor : review entity

--- src/review/entities/review.entity.ts
+++ src/review/entities/review.entity.ts
@@ -1,1 +1,15 @@
-export class ReviewEntity {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+@Schema()
+export class Review extends Document {
+  @Prop({ required: true, type: String })
+  comment: string;
+
+  @Prop({ required: true, type: Number })
+  rating: number; // Rating out of 5
+
+//   @Prop({ type: Types.ObjectId, ref: Course.name }) // Reference to Course model
+//   course: Types.ObjectId;
+}
+
+export const ReviewSchema = SchemaFactory.createForClass(Review);
