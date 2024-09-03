b05549ae4678b84f94ec9d92b189e5284c9b526a
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-03 15:51:03 +0300

    feat : add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  // Apply validation globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip properties that do not have any decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+      transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+    })
+  );
+  // const configService = app.get<ConfigService>(ConfigService);
+  // const expressInstance = app.getHttpAdapter().getInstance();
+  // useNestTreblle(expressInstance, {
+  //   apiKey: configService.get<string>('TREBLLE_API_KEY'),
+  //   projectId: configService.get<string>('TREBLLE_PROJECT_ID'),
+    
+  // });
+ 
+  const config = new DocumentBuilder()
+    .setTitle("Plan-B-API")
+    .setDescription("the description of the api")
+    .setVersion("1.0")
+    .build();
+  // const Document= SwaggerModule.createDocument(app,config) 
+  // SwaggerModule.setup('/DOC',app,Document)
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/swagger", app, document, {
+    customCssUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js",
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
