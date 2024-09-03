f76b9626cb9ffe654a6bef0ee3bab179e510fa6d
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-03 17:51:11 +0300

    refactor : faqs entity

--- src/faqs/entities/faq.entity.ts
+++ src/faqs/entities/faq.entity.ts
@@ -1,1 +1,16 @@
-export class FaqEntity{}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class Faq extends Document {
+  @Prop({ required: true })
+  question: string;
+
+  @Prop({ required: true })
+  answer: string;
+
+//   @Prop({ type: Types.ObjectId, ref: Course.name, nullable: true })
+//   course?: Types.ObjectId; // Reference to Course document
+}
+
+export const FaqSchema = SchemaFactory.createForClass(Faq);
