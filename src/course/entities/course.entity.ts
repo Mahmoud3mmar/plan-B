6f5d5a543af2d2f5451f494ea460a6f8c9cc9596
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-03 17:51:00 +0300

    refactor : course entity

--- src/course/entities/course.entity.ts
+++ src/course/entities/course.entity.ts
@@ -1,1 +1,39 @@
-export class CourseEntity {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+
+@Schema({ timestamps: true })
+export class Course extends Document {
+  @Prop({ required: true, maxlength: 255 })
+  name: string;
+
+  @Prop({ required: true })
+  overview: string;
+
+  @Prop({ required: true })
+  duration: number; // Duration in hours
+
+  @Prop({ required: true })
+  studentsEnrolled: number;
+
+  @Prop({ 
+    type: String, 
+    enum: ['Beginner', 'Intermediate', 'Expert', 'All Levels'], 
+    default: 'All Levels' 
+  })
+  level: 'Beginner' | 'Intermediate' | 'Expert' | 'All Levels';
+
+  @Prop({ required: true })
+  numberOfLessons: number;
+
+  @Prop({ required: true })
+  numberOfQuizzes: number;
+
+  @Prop({ type: [String], required: true })
+  curriculum: string[]; // Array of curriculum items
+
+  @Prop({type: Number, required: true })
+  price: number; // Decimal value, represented as a number
+}
+
+export const CourseSchema = SchemaFactory.createForClass(Course);
