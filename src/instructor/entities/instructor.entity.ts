00f63ff4b80d3ed3a6baf732206d35e272c2018f
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-04 15:26:11 +0300

    refactor: instructor entity

--- src/instructor/entities/instructor.entity.ts
+++ src/instructor/entities/instructor.entity.ts
@@ -1,22 +1,32 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
-
+import { User } from '../../user/entities/user.entity';
+// Define the schema for the Instructor
 @Schema()
-export class Instructor extends Document {
-  @Prop({ required: true, length: 255 })
+export class Instructor extends User {
+  @Prop({ required: true })
   name: string;
 
-  @Prop({ required: true, type: String })
+  @Prop({ required: true })
   bio: string;
 
-  @Prop({ type: [String], default: [] })
-  socialMediaUrls: string[];
+  @Prop({ required: true })
+  qualifications: string;
 
-  @Prop({ type: Number, default: 0 })
-  numberOfCourses: number;
+  @Prop({ required: true })
+  experience: string; // e.g., '5 years of experience in teaching Python'
 
-//   @Prop({ type: [{ type: Types.ObjectId, ref: Course.name }], default: [] })
-//   courses: Types.ObjectId[]; // Array of references to Course documents
+  @Prop({ required: true, default: 0 })
+  numberOfStudentsEnrolled: number; // Number of students enrolled with this instructor
+
+  @Prop({ required: true, default: 0 })
+  numberOfCoursesProvided: number; // Number of courses the instructor provides
+
+  @Prop({ type: [Types.ObjectId], ref: 'Course' })
+  courses: Types.ObjectId[]; // Array of course IDs provided by this instructor
+
+  @Prop({ type: [Types.ObjectId], ref: 'User' })
+  students: Types.ObjectId[]; // Array of student IDs enrolled in this instructor's courses
 }
 
 export const InstructorSchema = SchemaFactory.createForClass(Instructor);
