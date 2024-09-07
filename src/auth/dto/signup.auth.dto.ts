a1e3b4f731d28af74fbc011e9b3aa9e915b546f0
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-07 19:45:04 +0300

    refactor : auth dto

--- src/auth/dto/signup.auth.dto.ts
+++ src/auth/dto/signup.auth.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, MinLength, MaxLength, IsEmail, IsEnum } from 'class-validator';
+import { IsString, MinLength, MaxLength, IsEmail, IsEnum, ValidateIf, IsNotEmpty, ValidationArguments } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '../../user/common utils/Role.enum';
 
@@ -27,11 +27,37 @@ export class SignUpAuthDto {
   @ApiProperty({ description: 'The password of the user' })
   @IsString()
   @MinLength(4)
-  // Uncomment the next line if you want to add a regex password validation
-  // @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: 'Password is too weak' })
+  
   readonly password: string;
+
+
+  @ApiProperty({ description: 'Confirm password for the user' })
+  @IsString()
+  @MinLength(4)
+  @ValidateIf(o => o.password)
+  @IsNotEmpty({ message: 'Confirm password should not be empty' })
+  readonly confirmPassword: string;
 
   @ApiProperty({ description: 'The role of the user' })
   @IsEnum(Role, { message: 'Role must be either ADMIN, USER, etc.' })
   readonly role: Role;
+
+
+
+  
+}
+
+// Custom validation class to match passwords
+import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
+
+@ValidatorConstraint({ name: 'matchPasswords', async: false })
+export class MatchPasswords implements ValidatorConstraintInterface {
+  validate(confirmPassword: string, args: ValidationArguments) {
+    const object = args.object as SignUpAuthDto;
+    return object.password === confirmPassword;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return 'Password and confirm password must match';
+  }
 }
