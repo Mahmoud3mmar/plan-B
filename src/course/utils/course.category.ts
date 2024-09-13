85d86988127f1f2f47b423c3df683fb870886a90
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-14 00:48:57 +0300

    refactor : course utils

---
+++ src/course/utils/course.category.ts
@@ -0,0 +1,12 @@
+export enum CourseCategory {
+    Anatomy = 'Anatomy',
+    Biology = 'Biology',
+    BachelorOfScienceInNursing = 'Bachelor of Science in Nursing',
+    MedicalEthics = 'Medical Ethics',
+    Nursing = 'Nursing',
+    Physiotherapy = 'Physiotherapy',
+    Psychiatry = 'Psychiatry',
+    Psychology = 'Psychology',
+    Biochemistry = 'Biochemistry',
+  }
+  
