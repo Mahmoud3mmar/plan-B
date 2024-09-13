ee9cb4c25f76888c0608a2ec78114aa9581d970b
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-14 00:50:40 +0300

    refactor : instructor dto

--- src/instructor/dto/update.instructor.dto.ts
+++ src/instructor/dto/update.instructor.dto.ts
@@ -1,67 +1,38 @@
-import { IsOptional, IsString, IsArray, ValidateNested } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, IsUrl, IsArray, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
 class SocialMediaLinkDto {
-  @ApiProperty({
-    description: 'Type of the social media platform (e.g., Twitter, LinkedIn)',
-    example: 'Twitter',
-    required: false,
-  })
-  @IsOptional()
   @IsString()
-  type?: string;
+  readonly type: string;
 
-
-
-  @ApiProperty({ description: 'Profile image URL', required: false })
-  @IsString()
-  @IsOptional()
-  profileImage?: string;
-  @ApiProperty({
-    description: 'URL of the social media profile',
-    example: 'https://twitter.com/instructor',
-    required: false,
-  })
-  @IsOptional()
-  @IsString()
-  url?: string;
-
-
-  
+  @IsUrl()
+  readonly url: string;
 }
 
 export class UpdateInstructorDto {
-  @ApiProperty({
-    description: 'Name of the instructor',
-    example: 'John Doe',
-    required: false,
-  })
   @IsOptional()
   @IsString()
-  name?: string;
+  readonly firstName?: string;
 
-  @ApiProperty({
-    description: 'Bio of the instructor',
-    example: 'Experienced instructor in computer science with 10 years of teaching experience.',
-    required: false,
-  })
   @IsOptional()
   @IsString()
-  bio?: string;
+  readonly lastName?: string;
 
-  @ApiProperty({
-    description: 'Array of social media links with type and URL',
-    type: [SocialMediaLinkDto],
-    example: [
-      { type: 'Twitter', url: 'https://twitter.com/instructor' },
-      { type: 'LinkedIn', url: 'https://linkedin.com/in/instructor' },
-    ],
-    required: false,
-  })
+  @IsOptional()
+  @IsString()
+  readonly phoneNumber?: string;
+
+  @IsOptional()
+  @IsUrl()
+  readonly profileImage?: string;
+
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => SocialMediaLinkDto)
-  socialMediaLinks?: SocialMediaLinkDto[];
+  readonly socialMediaLinks?: SocialMediaLinkDto[];
+
+  @IsOptional()
+  @IsString()
+  readonly bio?: string;
 }
