51fcba3a886356d3231ca228fc40419b6401a84c
Mahmoud Ammar <55957447+Mahmoud3mmar@users.noreply.github.com>
2024-12-17 15:45:05 +0200

    refactor: Remove description field from CreateSummerTrainingDto
    - Removed the 'description' property from CreateSummerTrainingDto to simplify the data structure.
    - Updated related validation and API documentation accordingly.
    - Kept the image property for handling file uploads.

--- src/summertraining/dto/create.summertraining.dto.ts
+++ src/summertraining/dto/create.summertraining.dto.ts
@@ -7,10 +7,7 @@ export class CreateSummerTrainingDto {
   @IsNotEmpty()
   name: string;
 
-  @ApiProperty({ description: 'Description of the summer training' })
-  @IsString()
-  @IsNotEmpty()
-  description: string;
+
 
   @ApiProperty({ description: 'Image file for the summer training', type: 'string', format: 'binary' })
   image?: Express.Multer.File; // Optional if you want to handle it in the controller
